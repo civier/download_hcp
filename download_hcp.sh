7c475508f4508d390bcef99928c904fb89b8f46a
Oren Civier <civier@users.noreply.github.com>
2019-02-23 03:41:41 +1100

    Update download_hcp.sh

--- download_hcp.sh
+++ download_hcp.sh
@@ -7,18 +7,18 @@
 #
 #  Usage: download_hcp.sh <subject_list> <file_list>
 #
-#		subject_list - the name of the file containing subject 
-#				    numbers (one subject number per line)
-#		file_list - the name of the file containing HCP full
-#				 file names (one file name per line)
-#				 NOTICE: file names must include full path
-#					   relative to the base of each HCP
-#					   subject directory
+#		subject_list -  the name of the file containing subject 
+#				numbers (one subject number per line)
+#		file_list - 	the name of the file containing HCP full
+#				file names (one file name per line)
+#				NOTICE: file names must include full path
+#					relative to the base of each HCP
+#					subject directory
 #
 #  Output: For every subject, a subdirectory with the subject 
-#	      number will be created below the current directory.
-#		For each subject directory, the required files will be
-#		downloaded from the HCP repository on Amazon S3
+#	   number will be created below the current directory.
+#	   For each subject directory, the required files will be
+#	   downloaded from the HCP repository on Amazon S3
 #
 #################################################################
 
